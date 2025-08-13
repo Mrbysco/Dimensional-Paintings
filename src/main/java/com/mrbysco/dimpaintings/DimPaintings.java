a91935fddfc05c539d7d62e2c57100518e2388a8
Mrbysco <Mrbysco@users.noreply.github.com>
2025-08-13 15:11:14 +0200

    Enable in-game config

--- src/main/java/com/mrbysco/dimpaintings/DimPaintings.java
+++ src/main/java/com/mrbysco/dimpaintings/DimPaintings.java
@@ -12,6 +12,8 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig.Type;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import net.neoforged.neoforge.common.NeoForge;
 import org.slf4j.Logger;
 
@@ -33,6 +35,7 @@ public class DimPaintings {
 		NeoForge.EVENT_BUS.register(new CooldownHandler());
 
 		if (dist.isClient()) {
+			container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
 			eventBus.addListener(ClientHandler::onRegisterReloadListeners);
 			eventBus.addListener(ClientHandler::registerEntityRenders);
 		}
--- src/main/java/com/mrbysco/dimpaintings/datagen/client/DimensionalLanguageProvider.java
+++ src/main/java/com/mrbysco/dimpaintings/datagen/client/DimensionalLanguageProvider.java
@@ -4,6 +4,7 @@ import com.mrbysco.dimpaintings.DimPaintings;
 import com.mrbysco.dimpaintings.registry.PaintingRegistry;
 import net.minecraft.data.PackOutput;
 import net.neoforged.neoforge.common.data.LanguageProvider;
+import org.jetbrains.annotations.Nullable;
 
 public class DimensionalLanguageProvider extends LanguageProvider {
 	public DimensionalLanguageProvider(PackOutput packOutput) {
@@ -21,5 +22,26 @@ public class DimensionalLanguageProvider extends LanguageProvider {
 		add("dimpaintings.same_dimension", "Can't teleport to the same dimension");
 		add("dimpaintings.cooldown", "Teleportation on cooldown");
 		addEntityType(PaintingRegistry.DIMENSIONAL_PAINTING, "Dimensional Painting");
+
+		addConfig("General", "General", "General Settings");
+		addConfig("overworldToBed", "Overworld to Bed", "Dictates if the overworld painting will teleport you back to your spawnpoint / bed (Similar to teleporting from the End to the Overworld)");
+		addConfig("netherMaxY", "Nether Max Y", "Dictates the max Y at which the Nether Painting will place you in the Nether\n" +
+				"[For a vanilla nether it's recommended to keep the value between 10 and 120\n" +
+				"[51 = Spawn in a bubble at Y 50 if nothing] (Default: 120)");
+		addConfig("teleportCooldown", "Teleport Cooldown", "Amount of seconds between being able to teleport (Default: 4)");
+		addConfig("disableNetherPortal", "Disable Nether Portal", "Disable nether portal creation (Default: false)");
+	}
+
+	/**
+	 * Add the translation for a config entry
+	 *
+	 * @param path        The path of the config entry
+	 * @param name        The name of the config entry
+	 * @param description The description of the config entry (optional in case of targeting "title" or similar entries that have no tooltip)
+	 */
+	private void addConfig(String path, String name, @Nullable String description) {
+		this.add(DimPaintings.MOD_ID + ".configuration." + path, name);
+		if (description != null && !description.isEmpty())
+			this.add(DimPaintings.MOD_ID + ".configuration." + path + ".tooltip", description);
 	}
 }
